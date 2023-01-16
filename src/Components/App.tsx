c6a15bd8d347b3419d3b14dfe1a023c91daf87f3
JangEunJeong <cuttlefish77777@gmail.com>
2023-01-16 22:09:23 +0900

    [Feat] 추가 board간  DnD 코드

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -10,57 +10,74 @@ function App() {
     const {
       source: { droppableId: startBoardId, index: startIndex },
       destination: endBoard,
+      type,
     } = info;
     console.log(info);
+    const sameBoard = endBoard?.droppableId === startBoardId;
+    const otherBoard = endBoard?.droppableId !== startBoardId;
 
     if (!endBoard) return;
-    if (endBoard?.droppableId === startBoardId) {
+    if (type === "BOARD") {
       setBoards((allBoards) => {
         const boardCopy = [...allBoards];
-        const targetIndex = boardCopy.findIndex(
-          ({ title }) => title === startBoardId
-        );
-        const targetCards = [...boardCopy[targetIndex].content];
-        const targetCard = targetCards[startIndex];
-        // console.log(targetCard);
-        const [removed] = targetCards.splice(startIndex, 1);
-        targetCards.splice(endBoard?.index, 0, targetCard);
-        boardCopy[targetIndex] = {
-          title: startBoardId,
-          content: [...targetCards],
-        };
-        return [...boardCopy];
-      });
-    } else if (endBoard?.droppableId !== startBoardId) {
-      setBoards((allBoards) => {
-        const boardCopy = [...allBoards];
-
-        const firstIndex = boardCopy.findIndex(
-          ({ title }) => title === startBoardId
-        );
-        const firstBoard = [...boardCopy[firstIndex].content];
-        const firstCard = firstBoard[startIndex];
-
-        const finishedIndex = boardCopy.findIndex(
-          ({ title }) => title === endBoard.droppableId
-        );
-        const finishedBoard = [...boardCopy[finishedIndex].content];
-        firstBoard.splice(startIndex, 1);
-        finishedBoard.splice(endBoard.index, 0, firstCard);
-
-        console.log(firstBoard, finishedBoard);
-        boardCopy[firstIndex] = {
-          title: startBoardId,
-          content: [...firstBoard],
-        };
-        boardCopy[finishedIndex] = {
-          title: endBoard.droppableId,
-          content: [...finishedBoard],
-        };
-        return [...boardCopy];
+        const targetIndex = boardCopy[startIndex];
+        console.log(targetIndex);
+        boardCopy.splice(startIndex, 1);
+        boardCopy.splice(endBoard.index, 0, targetIndex);
+        return boardCopy;
       });
     }
+    switch (type === "CARD") {
+      case sameBoard:
+        setBoards((allBoards) => {
+          const boardCopy = [...allBoards];
+          const targetIndex = boardCopy.findIndex(
+            ({ title }) => title === startBoardId
+          );
+          const targetCards = [...boardCopy[targetIndex].content];
+          const targetCard = targetCards[startIndex];
+          // console.log(targetCard);
+          const [removed] = targetCards.splice(startIndex, 1);
+          targetCards.splice(endBoard?.index, 0, targetCard);
+          boardCopy[targetIndex] = {
+            title: startBoardId,
+            content: [...targetCards],
+          };
+          return [...boardCopy];
+        });
+        break;
+      case otherBoard:
+        setBoards((allBoards) => {
+          const boardCopy = [...allBoards];
+
+          const firstIndex = boardCopy.findIndex(
+            ({ title }) => title === startBoardId
+          );
+          const firstBoard = [...boardCopy[firstIndex].content];
+          const firstCard = firstBoard[startIndex];
+
+          const finishedIndex = boardCopy.findIndex(
+            ({ title }) => title === endBoard.droppableId
+          );
+          const finishedBoard = [...boardCopy[finishedIndex].content];
+          firstBoard.splice(startIndex, 1);
+          finishedBoard.splice(endBoard.index, 0, firstCard);
+
+          console.log(firstBoard, finishedBoard);
+          boardCopy[firstIndex] = {
+            title: startBoardId,
+            content: [...firstBoard],
+          };
+          boardCopy[finishedIndex] = {
+            title: endBoard.droppableId,
+            content: [...finishedBoard],
+          };
+          return [...boardCopy];
+        });
+        break;
+    }
   };
+
   return (
     <>
       <DragDropContext onDragEnd={onDragEnd}>
