34d237418cfd26047359f91d68b957d2d3ac80a8
JangEunJeong <cuttlefish77777@gmail.com>
2023-01-27 23:28:09 +0900

    [Design] 체크박스 토글시 변화

--- src/Components/AddToModal/CheckList.tsx
+++ src/Components/AddToModal/CheckList.tsx
@@ -32,6 +32,7 @@ const CheckList = ({
   const { register, handleSubmit, setFocus, setValue } = useForm<IListForm>();
   const { checkList } = cardContent;
   const [toggleAddList, setToggleAddList] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
   useEffect(() => {
     setFocus("list");
   }, [toggleAddList, setFocus]);
@@ -44,8 +45,9 @@ const CheckList = ({
       const newCheckList = {
         checkId: Date.now(),
         value: list,
-        state: false,
+        state: isChecked,
       };
+      console.log(newCheckList);
       const { checkList, ...rest } = targetBoardContent[cardIndex];
       targetBoardContent[cardIndex] = {
         ...rest,
@@ -62,6 +64,7 @@ const CheckList = ({
     }
   };
   const handleAddListState = () => setToggleAddList((prev) => !prev);
+
   const onCheckState = (itemId: number) => {
     setBoards((allBoards) => {
       const copyBoards = JSON.parse(JSON.stringify(allBoards));
@@ -69,7 +72,13 @@ const CheckList = ({
       const list = copyBoards[boardIndex].content[cardIndex].checkList;
       const newState = list.map((item: ICheckListType) => {
         if (itemId === item.checkId) {
-          return { ...item, state: !item.state };
+          const newObj = {
+            ...item,
+            state: !item.state,
+          };
+          setIsChecked(!item.state);
+          console.log(isChecked);
+          return newObj;
         } else {
           return item;
         }
@@ -84,6 +93,7 @@ const CheckList = ({
   };
   const stateArr = checkList?.map((item) => item.state);
   const currentState = stateArr.filter((item) => item);
+  console.log(stateArr);
   return (
     <CheckWrapper>
       <ContentTitleArea>
@@ -109,14 +119,14 @@ const CheckList = ({
         </ProgressBar>
         {checkList?.map((item, index) => (
           <ListView key={cardId + index}>
-            <span>{item.state === true ? "chcked" : ""}</span>
             <ListItem>
-              <span
+              <CheckBox
                 onClick={() => onCheckState(item.checkId)}
-                role="presentation"
+                bgColor={item.state ? "#4492bf" : "#fafbfc"}
               >
-                {item.value}
-              </span>
+                <Checked isChecked={item.state ? "1" : "0"} />
+              </CheckBox>
+              <span>{item.value}</span>
             </ListItem>
           </ListView>
         ))}
@@ -173,7 +183,6 @@ const ContentBox = styled.div`
 const ListView = styled.form`
   display: flex;
   align-items: center;
-
   padding-left: 40px;
   position: relative;
   transform-origin: left bottom;
@@ -182,29 +191,34 @@ const ListView = styled.form`
     background-color;
   transition-timing-function: ease-in;
 `;
-const CheckBox = styled.div`
-  border-radius: 4px;
+const CheckBox = styled.div<{ bgColor: string }>`
+  background-color: ${({ bgColor }) => bgColor};
+  border-radius: 2px;
+  box-shadow: inset 0 0 0 2px #dfe1e6;
+  cursor: pointer;
   flex-shrink: 0;
   height: 16px;
-  left: -6px;
+  left: -5px;
   margin: 6px;
   overflow: hidden;
   position: absolute;
   text-align: center;
-  top: 0;
+  top: 1px;
   transition: all 0.2s ease-in-out;
   white-space: nowrap;
   width: 16px;
-  display: block;
-  input[type="checkbox"] {
-    margin: 0;
-    vertical-align: middle;
-  }
+`;
+const Checked = styled.span<{ isChecked: string }>`
+  display: inline-block;
+  height: 16px;
+  width: 16px;
+  opacity: ${({ isChecked }) => isChecked};
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='%23fff' viewBox='-3 -4 16 16'%3E%3Cpath d='M1.49 3.215a.667.667 0 0 0-.98.903l2.408 2.613c.358.351.892.351 1.223.02l.243-.239a1689.645 1689.645 0 0 0 2.625-2.589l.027-.026a328.23 328.23 0 0 0 2.439-2.429.667.667 0 1 0-.95-.936c-.469.476-1.314 1.316-2.426 2.417l-.027.026a1368.126 1368.126 0 0 1-2.517 2.482L1.49 3.215Z'/%3E%3C/svg%3E");
 `;
 const ListItem = styled.div`
   overflow-wrap: break-word;
   word-break: break-word;
-  padding: 6px 0;
+  padding: 8px 0;
   width: 100%;
   span {
     min-height: 20px;
@@ -219,7 +233,7 @@ const ProgressBar = styled.div`
     line-height: 10px;
     position: absolute;
     text-align: center;
-    top: -1px;
+    top: 1px;
     width: 32px;
   }
   .progress-bar {
