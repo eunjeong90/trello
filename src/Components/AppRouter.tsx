8d8667b267cf4df9df11442b37e3497f32b8aaab
JangEunJeong <cuttlefish77777@gmail.com>
2023-01-31 15:42:04 +0900

    [Fix] 빌드시 절대경로 url

--- src/Components/AppRouter.tsx
+++ src/Components/AppRouter.tsx
@@ -1,11 +1,13 @@
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import Main from "./Main";
 
 const AppRouter = () => {
   return (
-    <Route path="/">
-      <Main />
-    </Route>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Route path="/">
+        <Main />
+      </Route>
+    </BrowserRouter>
   );
 };
 
